ea97572332169822080ab146320b7525e269e937
Eduard Sayakhov <eduardspektr@gmail.com>
2025-09-16 23:19:52 +0300

    feat: settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
+  base: '/star-wars/',
   test: {
     environment: 'happy-dom',
     setupFiles: './src/test/setup.ts',
